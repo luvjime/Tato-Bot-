4ad5c7a4c761a1572573eb5e3433a1faa057760a
luvjime <jimena.acosta.rubio@icloud.com>
2026-05-05 04:23:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 async function registerCommands() {
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+   Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID) ,
     { body: commands }
   );
   console.log("Slash commands registered.");
